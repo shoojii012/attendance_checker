fa9bece48b77cb3b1d62db61a5557d81d925a3a7
shoojii012 <shoji_yoshifumi@keio.jp>
2024-04-10 14:53:45 +0900

    正規表現の修正

--- make_rouch.py
+++ make_rouch.py
@@ -7,7 +7,7 @@ with open("log.csv") as f:
     log = [row for row in  reader]
 clipped_log = []
 for i in range(len(log)):
-    if log[i] == ["*","*","*"]:
+    if log[i] == [".*",".*",".*"]:
         clipped_log.append(log[i])
 print("test", clipped_log)
 # tdatetime = datetime.datetime.strptime(tstr,"%Y年%m月%d日 %H:%M:%S")
