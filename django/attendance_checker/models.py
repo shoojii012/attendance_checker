cffd78dee168d27c70aee70e675718d1b313ac4e
fmjp1012 <fmjp1012@gmail.com>
2024-05-28 19:13:48 +0900

    add models and migrations

--- django/attendance_checker/models.py
+++ django/attendance_checker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Device(models.Model):
+    mac_address = models.CharField(max_length=17, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    device_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Log(models.Model):
+    datetime = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
