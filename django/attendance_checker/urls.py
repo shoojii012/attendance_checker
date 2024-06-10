077efa3ff5ee74d10c6a263209f50dd177b57e6d
fmjp1012 <fmjp1012@gmail.com>
2024-06-11 00:10:29 +0900

    add ssg

--- django/attendance_checker/urls.py
+++ django/attendance_checker/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.statistics_view, name="statistics"),
-    path("ssg/", views.ssg, name="statistics"),
+    path("", views.ssg, name="statistics"),
     path("v1/", views.v1, name="v1"),
     # path("t", views.home, name="home"),
     # path("upload-csv/", views.upload_csv, name="upload_csv"),
